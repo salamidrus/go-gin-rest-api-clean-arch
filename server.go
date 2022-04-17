d781de335bf3aa3027536a1b56af4a6f843c2e6f
salamidrus <47554255+salamidrus@users.noreply.github.com>
2022-04-17 13:21:59 +0700

    Add Auth Controller and Routes

--- server.go
+++ server.go
@@ -1,13 +1,27 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/salamidrus/config"
+	"github.com/salamidrus/controller"
+
+	"gorm.io/gorm"
+)
+
+var (
+	db             *gorm.DB                  = config.SetupDatabaseConnection()
+	authController controller.AuthController = controller.NewAuthController()
+)
 
 func main() {
+	defer config.CloseDatabaseConnection(db)
 	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Hello World",
-		})
-	})
+
+	authRoutes := r.Group("api/auth")
+	{
+		authRoutes.POST("/login", authController.Login)
+		authRoutes.POST("/register", authController.Register)
+	}
+
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
